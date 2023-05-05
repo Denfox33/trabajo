beec7dbef9542eff5ea9df3f9523feead781cd54
Denfox33 <125207894+Denfox33@users.noreply.github.com>
2023-05-05 19:57:09 +0200

    Initial commit

---
+++ src/trabajo/Trabajo.java
@@ -0,0 +1,15 @@
+
+package trabajo;
+
+/**
+ *
+ * @author EAG
+ */
+public class Trabajo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
