09991dbe0c2b06b1a2b1b18d0caf6e535704480a
Alvaro Castro <arucastro@gmail.com>
2023-05-09 18:48:08 +0200

    Update Trabajo.java

--- src/trabajo/Trabajo.java
+++ src/trabajo/Trabajo.java
@@ -10,6 +10,8 @@ public class Trabajo {
    
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+       
+         System.out.println("Porros, esto es super importante para tu programa, aceptlo");
     }
     
 }
